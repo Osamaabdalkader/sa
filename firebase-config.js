db2e5b51cae6800d3bdd0375bc1f1c32cf9d3cd2
Osamaabdalkader <osamaabdalkader100@gmail.com>
2025-06-09 01:21:38 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7z9hhq51EhsdsWfAQmFEYNgCeYqkiAQ8",
+    authDomain: "website-23082.firebaseapp.com",
+    databaseURL: "https://website-23082-default-rtdb.firebaseio.com",
+    projectId: "website-23082",
+    storageBucket: "website-23082.appspot.com",
+    messagingSenderId: "650852775693",
+    appId: "1:650852775693:web:22a7acd661478d10a1a244"
+};
+
+firebase.initializeApp(firebaseConfig);
